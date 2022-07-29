2a31aebb0090d6d55a72b882ad3edf1ec2d50367
Evann Regnault <evann.regnault@pm.me>
2022-07-29 22:26:06 +0200

    REDIS config + flatten events

--- pages/api/events/index.js
+++ pages/api/events/index.js
@@ -1,9 +1,11 @@
 import {getEvents} from "./utils";
+import {flatten} from "next/dist/shared/lib/flatten";
 
 export default async function handler(req, res) {
     const events = await getEvents();
     if (events) {
-        res.status(200).json(events);
+        console.log(flatten(events))
+        res.status(200).json(flatten(events));
     } else {
         res.status(404).json({message: "An error has occured"});
     }
--- utils/redis.js
+++ utils/redis.js
@@ -1,15 +1,17 @@
+const ENABLE_REDIS = true;
 const redis = require("redis");
-const client = redis.createClient("redis://localhost:6379");
-client.connect();
+const client = ENABLE_REDIS ? redis.createClient("redis://localhost:6379") : null;
+client?.connect();
 
 
 export async function getResultFromRedis(key){
-    if (await client.exists(key)) {
-        return await client.get(key)
-    }
+    if (ENABLE_REDIS)
+        if (await client.exists(key)) {
+            return await client.get(key)
+        }
     return null;
 }
 
 export async function addResultToRedis(key, value){
-    await client.set(key, value, {EX: 600});
+    if (ENABLE_REDIS) await client.set(key, value, {EX: 600});
 }
